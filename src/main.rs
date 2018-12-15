9b01e3aaa3b95742fb7399bf9b1ba718922cb574
Chriscbr <crybicki98@gmail.com>
2018-12-14 20:56:06 -0500

    Fix bug caused by incorrect deref coercion

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate im;
 
 use im::HashMap;
 use std::rc::Rc;
+use std::ops::Deref;
 
 type Env = Rc<HashMap<String, Rc<Val>>>;
 
@@ -26,11 +27,11 @@ fn eval(env : Env, expr: &Expr) -> Rc<Val> {
         Expr::Id(x) => env.get(x).unwrap().clone(),
         Expr::App(e1, e2) => {
             let env_copy = env.clone();
-            match *eval(env, &*e1) {
+            match eval(env, &*e1).deref() {
                 Val::Clos(env2, x, body) => {
                     let v2 = eval(env_copy, &*e2);
                     let env2_copy = env2.clone();
-                    eval(Rc::new(env2_copy.update(x, v2)), &*body)
+                    eval(Rc::new(env2_copy.update(x.to_string(), v2)), &*body)
                 },
                 _ => panic!("no closure")
             }
@@ -58,6 +59,7 @@ mod tests {
 
     #[test]
     fn eval_bool() {
+        // evaluate a boolean value (true) to itself
         let env: Env = Rc::new(HashMap::new());
         let exp: Expr = Expr::Bool(true);
         let val: Rc<Val> = eval(env.clone(), &exp);
@@ -67,6 +69,7 @@ mod tests {
 
     #[test]
     fn eval_if() {
+        // evaluate an if expression which negates the value of x in the env
         let mut env_0: HashMap<String, Rc<Val>> = HashMap::new();
         env_0.insert(String::from("x"),
                      Rc::new(Val::Bool(false)));
@@ -74,6 +77,20 @@ mod tests {
         let exp: Expr = Expr::If(Box::new(Expr::Id(String::from("x"))),
                                  Box::new(Expr::Bool(false)),
                                  Box::new(Expr::Bool(true)));
+        let val: Rc<Val> = eval(env.clone(), &exp);
+        println!("{:?} evaluates to {:?}", exp, val);
+        assert_eq!(val, Rc::new(Val::Bool(true)));
+    }
+
+    #[test]
+    fn eval_apply() {
+        // apply a function which negates its argument, to the value "false"
+        let env: Env = Rc::new(HashMap::new());
+        let exp: Expr = Expr::App(Box::new(Expr::Fun(String::from("x"),
+                Box::new(Expr::If(Box::new(Expr::Id(String::from("x"))),
+                                  Box::new(Expr::Bool(false)),
+                                  Box::new(Expr::Bool(true)))))),
+            Box::new(Expr::Bool(false)));
         let val: Rc<Val> = eval(env.clone(), &exp);
         println!("{:?} evaluates to {:?}", exp, val);
         assert_eq!(val, Rc::new(Val::Bool(true)));
