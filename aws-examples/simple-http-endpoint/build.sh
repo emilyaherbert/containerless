53092933f73904a1c3c246020bec423113ff5b16
Arjun Guha <arjun@cs.umass.edu>
2019-04-11 12:36:11 -0400

    Removed old stuff

--- aws-examples/simple-http-endpoint/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This builds an executable with Amazon Linux as target and zips up the executable
-# to be uploaded to AWS Lambda
-cargo build --release --target x86_64-unknown-linux-musl && \
-zip -j lambda.zip ./target/x86_64-unknown-linux-musl/release/bootstrap
