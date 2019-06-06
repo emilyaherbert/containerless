7db0189c7d2c912c769cc88df2d361cceea9a52b
emilyaherbert <emilyherbert@cs.umass.edu>
2019-06-06 17:19:05 -0400

    google_datastore1 hello world

--- gcloud-trials/rs/README.md
+++ gcloud-trials/rs/README.md
@@ -2,9 +2,6 @@ https://cloud.google.com/run/docs/quickstarts/build-and-deploy?authuser=1
 https://cloud.google.com/run/docs/building/containers?authuser=1
 https://github.com/knative/docs/tree/175313457f94baa036b400f12d162157edef70a7/community/samples/serving/helloworld-rust
 
+# Rust + Cloud Datastore
 
-# Steps
-
-1. `cargo build`
-2. `gcloud builds submit --tag gcr.io/umass-plasma/helloworld --project umass-plasma`
-3. `gcloud beta run deploy --image gcr.io/umass-plasma/helloworld --project umass-plasma`
+https://cloud.google.com/datastore/docs/reference/data/rest/
--- gcloud-trials/rs/src/main.rs
+++ gcloud-trials/rs/src/main.rs
@@ -1,48 +1,57 @@
-#![deny(warnings)]
-
-// https://github.com/knative/docs/tree/175313457f94baa036b400f12d162157edef70a7/community/samples/serving/helloworld-rust
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
 
 extern crate hyper;
-extern crate pretty_env_logger;
-
-use hyper::{Body, Response, Server};
-use hyper::service::service_fn_ok;
-use hyper::rt::{self, Future};
-use std::env;
+extern crate hyper_rustls;
+extern crate yup_oauth2 as oauth2;
+extern crate google_datastore1 as datastore1;
+use datastore1::GoogleDatastoreAdminV1ExportEntitiesRequest;
+use datastore1::{Result, Error};
+use std::default::Default;
+use oauth2::{Authenticator, DefaultAuthenticatorDelegate, ApplicationSecret, MemoryStorage};
+use datastore1::Datastore;
 
 fn main() {
-    pretty_env_logger::init();
 
-    let mut port: u16 = 8080;
-    match env::var("PORT") {
-        Ok(p) => {
-            match p.parse::<u16>() {
-                Ok(n) => {port = n;},
-                Err(_e) => {},
-            };
-        }
-        Err(_e) => {},
-    };
-    let addr = ([0, 0, 0, 0], port).into();
+    // Get an ApplicationSecret instance by some means. It contains the `client_id` and 
+    // `client_secret`, among other things.
+    let secret: ApplicationSecret = Default::default();
+    // Instantiate the authenticator. It will choose a suitable authentication flow for you, 
+    // unless you replace  `None` with the desired Flow.
+    // Provide your own `AuthenticatorDelegate` to adjust the way it operates and get feedback about 
+    // what's going on. You probably want to bring in your own `TokenStorage` to persist tokens and
+    // retrieve them from storage.
+    let auth = Authenticator::new(&secret, DefaultAuthenticatorDelegate,
+                                hyper::Client::with_connector(hyper::net::HttpsConnector::new(hyper_rustls::TlsClient::new())),
+                                <MemoryStorage as Default>::default(), None);
+    let mut hub = Datastore::new(hyper::Client::with_connector(hyper::net::HttpsConnector::new(hyper_rustls::TlsClient::new())), auth);
+    // As the method needs a request, you would usually fill it with the desired information
+    // into the respective structure. Some of the parts shown here might not be applicable !
+    // Values shown here are possibly random and not representative !
+    let mut req = GoogleDatastoreAdminV1ExportEntitiesRequest::default();
+    
+    // You can configure optional parameters by calling the respective setters at will, and
+    // execute the final call using `doit()`.
+    // Values shown here are possibly random and not representative !
+    let result = hub.projects().export(req, "projectId")
+                .doit();
+    
+    match result {
+        Err(e) => match e {
+            // The Error enum provides details about what exactly happened.
+            // You can also just use its `Debug`, `Display` or `Error` traits
+            Error::HttpError(_)
+            |Error::MissingAPIKey
+            |Error::MissingToken(_)
+            |Error::Cancelled
+            |Error::UploadSizeLimitExceeded(_, _)
+            |Error::Failure(_)
+            |Error::BadRequest(_)
+            |Error::FieldClash(_)
+            |Error::JsonDecodeError(_, _) => println!("{}", e),
+        },
+        Ok(res) => println!("Success: {:?}", res),
+    }
 
-    let new_service = || {
-        service_fn_ok(|_| {
-
-            let mut hello = "Hello ".to_string();
-            match env::var("TARGET") {
-                Ok(target) => {hello.push_str(&target);},
-                Err(_e) => {hello.push_str("World")},
-            };
-
-            Response::new(Body::from(hello))
-        })
-    };
-
-    let server = Server::bind(&addr)
-        .serve(new_service)
-        .map_err(|e| eprintln!("server error: {}", e));
-
-    println!("Listening on http://{}", addr);
-
-    rt::run(server);
 }
