cf2bb80483c49ed2078503eab45425328726de26
emilyaherbert <emilyherbert@cs.umass.edu>
2019-07-16 15:08:28 -0400

    Function calls.

--- js-transform/ts/insertTracing.ts
+++ js-transform/ts/insertTracing.ts
@@ -1,13 +1,49 @@
 import * as t from '@babel/types';
 
+function identifier(s: string): t.Expression {
+    const callee = t.identifier('identifier');
+    const theArgs = [t.stringLiteral(s)];
+    return t.callExpression(callee, theArgs);
+}
+
+function number(n: number): t.Expression {
+    const callee = t.identifier('number');
+    const theArgs = [t.numericLiteral(n)];
+    return t.callExpression(callee, theArgs);
+}
+
+function boolean(b: boolean): t.Expression {
+    const callee = t.identifier('boolean');
+    const theArgs = [t.booleanLiteral(b)];
+    return t.callExpression(callee, theArgs);
+}
+
 function traceLet(theArgs: t.Expression[]): t.ExpressionStatement {
     const memberExpression = t.memberExpression(t.identifier('t'), t.identifier('traceLet'));
     const callExpression = t.callExpression(memberExpression, theArgs);
     return t.expressionStatement(callExpression);
 }
 
+// TODO(emily): Change this to LVal later.
+function traceFunctionCall(id: string, theArgs: t.Expression[]): t.ExpressionStatement {
+    const memberExpression = t.memberExpression(t.identifier('t'), t.identifier('traceFunctionCall'));
+    const memberArgs = [t.stringLiteral(id), t.arrayExpression(theArgs)];
+    const callExpression = t.callExpression(memberExpression, memberArgs);
+    return t.expressionStatement(callExpression);
+}
+
+function exitBlock(): t.ExpressionStatement {
+    const memberExpression = t.memberExpression(t.identifier('t'), t.identifier('exitBlock'));
+    const callExpression = t.callExpression(memberExpression, []);
+    return t.expressionStatement(callExpression);
+}
+
 function reifyExpression(e: t.Expression): t.Expression {
-    return e;
+    switch(e.type) {
+        case 'NumericLiteral': return number(e.value);
+        case 'BooleanLiteral': return boolean(e.value);
+        default: throw new Error("Type not implemented.")
+    }
 }
 
 function reifyVariableDeclaration(s: t.VariableDeclaration): t.Statement[] {
@@ -18,17 +54,31 @@ function reifyVariableDeclaration(s: t.VariableDeclaration): t.Statement[] {
     } else {
         switch(init.type) {
             case 'CallExpression': {
-                break;
+                if(!t.isIdentifier(init.callee)) {
+                    throw new Error("Expected id callee.");
+                }
+                const callee = init.callee.name;
+                let theArgs: t.Expression[] = [identifier(callee)];
+                init.arguments.forEach(a => {
+                    if(t.isSpreadElement(a)) {
+                        throw new Error("Found spread element.");
+                    } else if(t.isJSXNamespacedName(a)) {
+                        throw new Error("Found JSXNamespacedName.");
+                    } else if(t.isArgumentPlaceholder(a)) {
+                        throw new Error("Found argument placeholder.");
+                    }
+                    theArgs.push(reifyExpression(a));
+                })
+                const tCall = traceFunctionCall(lvaltoName(id), theArgs);
+                const tExit = exitBlock();
+                return [tCall, s, tExit];
             }
             default: {
-                const theArgs = [t.identifier(lvaltoName(id)), reifyExpression(init)];
-                const tLet = traceLet(theArgs);
+                const tLet = traceLet([t.identifier(lvaltoName(id)), reifyExpression(init)]);
                 return [tLet, s];
             }
         }
     }
-
-    return [t.expressionStatement(t.stringLiteral('TODO'))]
 }
 
 function reifyStatement(s: t.Statement): t.Statement[] {
