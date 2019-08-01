61f20a0eae2ed31b18610301a5a7e1cfb270add0
emilyaherbert <emilyherbert@cs.umass.edu>
2019-08-01 17:36:18 -0400

    Trying to fix CI

--- build.sh
+++ build.sh
@@ -2,5 +2,10 @@ set -x
 set -e
 cd /workspace/containerless
 yarn install
+cd /workspace/js-transform
+yarn install
+yarn link
+cd /workspace/containerless
+yarn link js-transform
 yarn run build
 yarn run test
