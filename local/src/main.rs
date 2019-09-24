f329ed6f4ce769d0ddb160da8e6dafd75fb21b95
emilyaherbert <emilyherbert@cs.umass.edu>
2019-09-24 18:55:51 -0400

    Mock filestore

--- local/src/main.rs
+++ local/src/main.rs
@@ -1,18 +1,35 @@
-use hyper::{Body, Response, Server};
+use hyper::{Body, Response, Server, Request, Method, StatusCode};
 use hyper::rt::Future;
-use hyper::service::service_fn_ok;
+use hyper::service::service_fn;
+use futures::future;
+
+// https://hyper.rs/
+
+type BoxFut = Box<dyn Future<Item=Response<Body>, Error=hyper::Error> + Send>;
+
+fn echo(req: Request<Body>) -> BoxFut {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            *response.body_mut() = Body::from("Try POSTing a file to /upload.\n");
+        },
+        (&Method::POST, "/upload") => {
+            *response.body_mut() = Body::from("Done uploading!\n");
+        },
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        },
+    };
+
+    Box::new(future::ok(response))
+}
 
 fn main() {
     let addr = ([127, 0, 0, 1], 3000).into();
 
-    let new_svc = || {
-        service_fn_ok(|_req|{
-            Response::new(Body::from("Hello, World!\n"))
-        })
-    };
-
     let server = Server::bind(&addr)
-        .serve(new_svc)
+        .serve(|| service_fn(echo))
         .map_err(|e| eprintln!("server error: {}", e));
 
     hyper::rt::run(server);
