6b34a50c39244347e58297091d5033bc47443ad1
emilyaherbert <emilyherbert@cs.umass.edu>
2019-09-24 18:44:09 -0400

    Starting working on local stuff that helps us build the functions for the paper - mostly for the uploading file function.

---
+++ local/src/main.rs
@@ -0,0 +1,19 @@
+use hyper::{Body, Response, Server};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+
+fn main() {
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let new_svc = || {
+        service_fn_ok(|_req|{
+            Response::new(Body::from("Hello, World!\n"))
+        })
+    };
+
+    let server = Server::bind(&addr)
+        .serve(new_svc)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    hyper::rt::run(server);
+}
