e189941ec2170313b88fa89b4a1d57202d6287ea
Arjun Guha <arjun@cs.umass.edu>
2019-09-30 06:43:51 -0400

    Add support for directly pinging the invoker
    This may be useful for another baseline

--- rust/invoker/src/server.rs
+++ rust/invoker/src/server.rs
@@ -11,14 +11,27 @@ pub fn serve(config: Config) -> impl future::Future<Item = (), Error = Error> {
 
     let client = Arc::new(Client::new());
     let config = Arc::new(config);
+    let client1 = client.clone();
 
     let pool = Arc::new(ContainerPool::new(config, client));
     let pool2 = Arc::clone(&pool);
 
     let new_svc = move || {
         let pool2 = Arc::clone(&pool2);
-        service_fn(move |req| {
-            ContainerPool::request(&pool2, req).map(move |(resp, _duration)| resp)
+        let client1 = client1.clone();
+        service_fn(move |mut req| {
+            if req.uri() == "/ping" {
+                let new_uri = hyper::Uri::builder()
+                .scheme("http")
+                .authority("localhost:8081")
+                .path_and_query(req.uri().path())
+                .build()
+                .unwrap();
+                *req.uri_mut() = new_uri;
+                future::Either::A(client1.request(req).from_err())
+            } else {
+                future::Either::B(ContainerPool::request(&pool2, req).map(move |(resp, _duration)| resp))
+            }
         })
     };
 
