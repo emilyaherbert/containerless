8a8d0f39366a77d96f8ea301851d07af521f5573
Arjun Guha <arjun@cs.umass.edu>
2019-10-04 06:17:31 -0400

    /ping responds directly from Rust

--- rust/invoker/src/server.rs
+++ rust/invoker/src/server.rs
@@ -7,7 +7,7 @@ use futures::{
     Future,
 };
 use hyper::service::service_fn;
-use hyper::{Client, Server};
+use hyper::{Client, Server, Response, Body};
 use std::sync::Arc;
 
 pub fn serve(config: Config) -> impl future::Future<Item = (), Error = Error> {
@@ -15,25 +15,17 @@ pub fn serve(config: Config) -> impl future::Future<Item = (), Error = Error> {
 
     let client = Arc::new(Client::new());
     let config = Arc::new(config);
-    let client1 = client.clone();
 
     let pool = Arc::new(ContainerPool::new(config.clone(), client));
     let pool2 = Arc::clone(&pool);
 
     let new_svc = move || {
         let pool2 = Arc::clone(&pool2);
-        let client1 = client1.clone();
         let config = config.clone();
-        service_fn(move |mut req| {
+        service_fn(move |req| {
             if req.uri() == "/ping" {
-                let new_uri = hyper::Uri::builder()
-                    .scheme("http")
-                    .authority("localhost:8081")
-                    .path_and_query(req.uri().path())
-                    .build()
-                    .unwrap();
-                *req.uri_mut() = new_uri;
-                Either::B(Either::A(client1.request(req).from_err()))
+                let resp = Response::new(Body::from("Hello from Rust (no container)"));
+                Either::B(Either::A(future::ok(resp)))
             } else if let Some(containerless) = config.containerless {
                 Either::A(Decontainer::new(containerless, req))
             } else {
