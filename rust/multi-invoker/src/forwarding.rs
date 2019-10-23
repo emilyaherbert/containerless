93240592464af357afb409fed64b44c5d372e9fa
emilyaherbert <emilyherbert@cs.umass.edu>
2019-10-22 21:41:09 -0400

    Trying out different iptables stuff.

--- rust/multi-invoker/src/forwarding.rs
+++ rust/multi-invoker/src/forwarding.rs
@@ -1,11 +1,16 @@
 use duct::cmd;
 use shared::OS;
 
+enum LinuxState {
+    First,
+    Alive
+}
+
 pub struct Forwarding {
     src_port: u16,
     dst_port: u16,
     os: shared::OS,
-    linux_flag: bool,
+    linux_state: LinuxState,
     prev_dst_port: u16
 }
 
@@ -18,15 +23,13 @@ impl Drop for Forwarding {
                 .expect("could not flush rules");
             }
             OS::Linux => {
-                if self.linux_flag {
-                    cmd!("sudo", "iptables", "-t", "nat", "-D", "PREROUTING", "-p", "tcp", "--dport", self.src_port.to_string(), "-j", "REDIRECT", "--to", self.dst_port.to_string())
-                    .run()
-                    .and_then(|_| {
-                        cmd!("sudo", "iptables", "-t", "nat", "-D", "OUTPUT", "-p", "tcp", "--dport", self.src_port.to_string(), "-j", "REDIRECT", "--to", self.dst_port.to_string())
+                match self.linux_state {
+                    LinuxState::Alive => {
+                        cmd!("sudo", "iptables", "-t", "nat", "-D", "PREROUTING", "-p", "tcp", "--dport", self.src_port.to_string(), "-j", "REDIRECT", "--to", self.dst_port.to_string())
                         .run()
-                    })
-                    .expect("Failed to drop previous rules.");
-                    self.linux_flag = false;
+                        .expect("could not flush rules");
+                    },
+                    LinuxState::First => panic!("Did not expect to see this here!")
                 }
             }
         }
@@ -40,7 +43,7 @@ impl Forwarding {
             src_port,
             dst_port,
             os,
-            linux_flag: false,
+            linux_state: LinuxState::First,
             prev_dst_port: dst_port
         };
         result.do_it();
@@ -65,24 +68,18 @@ impl Forwarding {
                 .expect("Failed to reconfigure firewall");
             }
             OS::Linux => {
-                cmd!("sudo", "iptables", "-t", "nat", "-A", "PREROUTING", "-p", "tcp", "--dport", self.src_port.to_string(), "-j", "REDIRECT", "--to", self.dst_port.to_string())
-                .run()
-                .and_then(|_| {
-                    cmd!("sudo", "iptables", "-t", "nat", "-A", "OUTPUT", "-p", "tcp", "--dport", self.src_port.to_string(), "-j", "REDIRECT", "--to", self.dst_port.to_string())
-                    .run()
-                })
-                .expect("Failed to reconfigure firewall");
-                if self.linux_flag {
-                    cmd!("sudo", "iptables", "-t", "nat", "-D", "PREROUTING", "-p", "tcp", "--dport", self.src_port.to_string(), "-j", "REDIRECT", "--to", self.prev_dst_port.to_string())
-                    .run()
-                    .and_then(|_| {
-                        cmd!("sudo", "iptables", "-t", "nat", "-D", "OUTPUT", "-p", "tcp", "--dport", self.src_port.to_string(), "-j", "REDIRECT", "--to", self.prev_dst_port.to_string())
+                match self.linux_state {
+                    LinuxState::First => {
+                        cmd!("sudo", "iptables", "-t", "nat", "-A", "PREROUTING", "-p", "tcp", "--dport", self.src_port.to_string(), "-j", "REDIRECT", "--to", self.dst_port.to_string())
                         .run()
-                    })
-                    .expect("Failed to drop previous rules.");
-                    self.linux_flag = false;
+                        .expect("Failed to reconfigure firewall");
+                    },
+                    LinuxState::Alive => {
+                        cmd!("sudo", "iptables", "-t", "nat", "-R", "PREROUTING", "2", "-p", "tcp", "--dport", self.src_port.to_string(), "-j", "REDIRECT", "--to", self.dst_port.to_string())
+                        .run()
+                        .expect("Failed to reconfigure firewall");
+                    }
                 }
-                self.linux_flag = true;
             }
         }
     }
