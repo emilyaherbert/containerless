9302d94d014c1c8afe0a1f1654b325cad88234cb
Emily Herbert <emilyherbert@cs.umass.edu>
2019-10-30 01:56:37 +0000

    Moving benchmark back and forth.

--- javascript/examples/upload.js
+++ javascript/examples/upload.js
@@ -4,15 +4,19 @@ containerless.listen(function(req) {
     if(req.path === '/upload') {
         if(req.body !== undefined) {
             containerless.post({
-                url: 'http://localhost:7999/upload',
+		url: 'http://10.200.0.1:7999/upload',
                 body: req.body
             }, function(resp) {
-                containerless.respond('Uploaded\n');
+	        if(resp.error !== undefined) {
+                    containerless.respond("error\n");
+                } else {
+                    containerless.respond('Uploaded\n');
+                }
             });
         } else {
             containerless.respond("No file to upload.\n");
         }
     } else {
-        containerless.respond("Unknown command.\n");
+        containerless.respond(req.path);
     }
 });
