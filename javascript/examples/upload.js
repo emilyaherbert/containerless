6bc726d4170c60822f9aeed400bd344b3c79bc6a
emilyaherbert <emilyherbert@cs.umass.edu>
2019-10-29 23:39:14 -0400

    Moving stuff back and forth.

--- javascript/examples/upload.js
+++ javascript/examples/upload.js
@@ -4,19 +4,19 @@ containerless.listen(function(req) {
     if(req.path === '/upload') {
         if(req.body !== undefined) {
             containerless.post({
-		url: 'http://10.200.0.1:7999/upload',
+		        url: 'http://10.200.0.1:7999/upload',
                 body: req.body
             }, function(resp) {
-	        if(resp.error !== undefined) {
+	            if(resp.error !== undefined) {
                     containerless.respond("error\n");
                 } else {
-                    containerless.respond('Uploaded\n');
+                    containerless.respond('Uploaded.\n');
                 }
             });
         } else {
             containerless.respond("No file to upload.\n");
         }
     } else {
-        containerless.respond(req.path);
+        containerless.respond("Unknown command.\n");
     }
 });
