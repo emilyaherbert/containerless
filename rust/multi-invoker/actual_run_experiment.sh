7e6369927741f1a18a659a3641b0823f2ba5da3b
Emily Herbert <emilyherbert@cs.umass.edu>
2019-10-31 02:38:24 +0000

    Changing log file destination directory.

--- rust/multi-invoker/actual_run_experiment.sh
+++ rust/multi-invoker/actual_run_experiment.sh
@@ -11,7 +11,7 @@ JS_FILE=$3
 # Run the invoker
 OUTPUT_PATH="./results/${JS_FILE}/${RUN_TYPE}"
 mkdir --parents $OUTPUT_PATH
-CONFIG1="{\"bind_port\":8081,\"image_name\":\"serverless-function\",\"max_containers\":6,\"initial_state\":\"Tracing\",\"utilization_log\":\"${OUTPUT_PATH}/${OPEN_CONNECTIONS}_1.log\",\"kill_parent\":true}"
-CONFIG2="{\"bind_port\":8082,\"image_name\":\"serverless-function\",\"max_containers\":6,\"initial_state\":\"Decontainerized\",\"utilization_log\":\"${OUTPUT_PATH}/${OPEN_CONNECTIONS}_2.log\",\"kill_parent\":true}"
+CONFIG1="{\"bind_port\":8081,\"image_name\":\"serverless-function\",\"max_containers\":6,\"initial_state\":\"Tracing\",\"utilization_log\":\"${OUTPUT_PATH}_C/${OPEN_CONNECTIONS}.log\",\"kill_parent\":true}"
+CONFIG2="{\"bind_port\":8082,\"image_name\":\"serverless-function\",\"max_containers\":6,\"initial_state\":\"Decontainerized\",\"utilization_log\":\"${OUTPUT_PATH}_D/${OPEN_CONNECTIONS}.log\",\"kill_parent\":true}"
 CONFIG="{\"bind_port\":8080,\"config_a\":${CONFIG1},\"config_b\":${CONFIG2}}"
 cargo run -- --config ${CONFIG}
