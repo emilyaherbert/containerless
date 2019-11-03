3546ea00faf558d832e04ab24e1300fe1c4285b8
emilyaherbert <emilyherbert@cs.umass.edu>
2019-11-02 21:30:42 -0400

    wrk2 exec files.

---
+++ rust/containerless-scaffold/run_utilization_experiment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# $1 run type (Decontainerized, DisableTracing)
+# $2 number of open connections
+# $3 name
+
+RUN_TYPE=$1
+PER_SEC="$2"
+JS_FILE=$3
+
+# Run the invoker
+OUTPUT_PATH="./results/wrk2/${RUN_TYPE}"
+mkdir --parents $OUTPUT_PATH
+CONFIG="{\"image_name\":\"serverless-function\",\"max_containers\":6,\"initial_state\":\"${RUN_TYPE}\",\"utilization_log\":\"${OUTPUT_PATH}/${PER_SEC}.log\"}"
+cargo run --release -- --config ${CONFIG}
