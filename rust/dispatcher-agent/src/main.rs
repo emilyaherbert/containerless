59395b8981e5ece365bf60a0ac58975a786462b7
Arjun Guha <arjun@cs.umass.edu>
2020-04-21 09:10:49 -0400

    Dispatcher now uses warp

--- rust/dispatcher-agent/src/main.rs
+++ rust/dispatcher-agent/src/main.rs
@@ -9,83 +9,72 @@ mod util;
 mod windowed_max;
 
 use function_table::FunctionTable;
-use hyper::service::{make_service_fn, service_fn};
-use hyper::Server;
-use log::{debug, error, info};
-use std::convert::Infallible;
-use std::sync::Arc;
 use tokio::signal::unix::{signal, SignalKind};
 use types::*;
+use warp::Filter;
 
-async fn handle_req(state: Arc<FunctionTable>, req: Request) -> Result<Response, hyper::Error> {
-    let (parts, body) = req.into_parts();
-    let path = parts.uri.path();
-    let mut split_path = path.splitn(3, '/');
-    let _ = split_path.next(); // Drop the leading /
-    match split_path.next() {
-        Some("readinessProbe") => {
-            return Ok(hyper::Response::builder()
-                .status(200)
-                .body(hyper::Body::from(
-                    "To invoke: http://HOSTNAME/dispatcher/FUNCTION-NAME\n",
-                ))
-                .unwrap());
-        }
-        Some(function_name) => {
-            let function_path = format!("/{}", split_path.next().unwrap_or(""));
-            debug!(target: "dispatcher", "received request for function {} with path {}", function_name, function_path);
-            let mut fm = FunctionTable::get_function(&state, function_name).await;
-            debug!(target: "dispatcher", "invoking function {} with path {}", function_name, function_path);
-            let resp = fm.invoke(parts.method, &function_path, body).await?;
-            return Ok(resp);
-        }
-        None => {
-            return Ok(hyper::Response::builder()
-                .status(404)
-                .body(hyper::Body::from(
-                    "To invoke: http://HOSTNAME/dispatcher/FUNCTION-NAME\n",
-                ))
-                .unwrap());
-        }
-    }
+async fn readiness_handler() -> Result<impl warp::Reply, warp::Rejection> {
+    return Ok(hyper::Response::builder()
+        .status(200)
+        .body(hyper::Body::from(
+            "To invoke: http://HOSTNAME/dispatcher/FUNCTION-NAME\n",
+        ))
+        .unwrap());
+}
+
+async fn dispatcher_handler(function_name: String, mut function_path: String, method: http::Method, body: bytes::Bytes, state: Arc<FunctionTable>) -> Result<impl warp::Reply, warp::Rejection>  {
+    debug!(target: "dispatcher", "received request for function {} with path {}", function_name, function_path);
+    let mut fm = FunctionTable::get_function(&state, &function_name).await;
+    debug!(target: "dispatcher", "invoking function {} with path {}", function_name, function_path);
+    let body = hyper::Body::from(body);
+    function_path = format!("/{}", function_path);
+    return match fm.invoke(method, &function_path, body).await {
+        Ok(resp) => Ok(resp),
+        Err(err) =>
+            Ok(hyper::Response::builder()
+            .status(500)
+            .body(hyper::Body::from(format!("Error invoking function {}", err)))
+            .unwrap())
+    };
 }
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
 
-    info!(target: "dispatcher", "Starting dispatcher");
+    info!(target: "dispatcher", "Started dispatcher");
     let state = FunctionTable::new().await;
     if let Err(err) = FunctionTable::adopt_running_functions(&state).await {
         error!(target: "dispatcher", "adopting functions: {}", err);
         return;
     }
 
-    let make_svc = {
+    let extract_state = {
         let state = state.clone();
-        make_service_fn(move |_conn| {
-            let state = state.clone();
-            futures::future::ok::<_, Infallible>(service_fn(move |req| {
-                handle_req(state.clone(), req)
-            }))
-        })
+        warp::any().map(move || state.clone())
     };
+    
+    let readiness_route = warp::get()
+        .and(warp::path!("readinessProbe"))
+        .and_then(readiness_handler);
 
-    let addr = ([0, 0, 0, 0], 8080).into();
+    let dispatcher_route = warp::path!(String / String)
+        .and(warp::method())
+        .and(warp::filters::body::bytes())
+        .and(extract_state)
+        .and_then(dispatcher_handler);
 
-    let server = Server::bind(&addr).serve(make_svc);
+    let paths = readiness_route
+        .or(dispatcher_route);
 
-    let addr = server.local_addr();
-    info!(target: "dispatcher", "listening on port {}", addr.port());
-    server
-        .with_graceful_shutdown(async {
+    info!(target: "dispatcher", "started listening");
+    let (_addr, server) = warp::serve(paths)
+        .bind_with_graceful_shutdown(([0, 0, 0, 0], 8080), async {
             let mut sigterm = signal(SignalKind::terminate()).expect("registering SIGTERM handler");
             sigterm.recv().await;
             println!("Received SIGTERM");
-        })
-        .await
-        .expect("starting server");
-
+        });
+    server.await;
     FunctionTable::orphan(state).await;
     std::process::exit(0);
 }
