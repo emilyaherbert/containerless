be506ca333868248a0b765d7bc9603cdb0d14b9c
Emily Herbert <emilyherbert@cs.umass.edu>
2020-04-30 00:04:31 +0000

    Got a test to pass. Other tests fail.

--- rust/integration-tests/src/tests.rs
+++ rust/integration-tests/src/tests.rs
@@ -37,3 +37,58 @@ fn loops() {
     );
     assert_eq!(results, vec!["3", "3"]);
 }
+
+#[test]
+fn yay() {
+    let results = run_test(
+        "yay",
+        r#"
+        let containerless = require("containerless");
+        containerless.listen(function(req) {
+            containerless.respond("yay!");
+        });"#,
+        vec![("/hello", json!({}))],
+        vec![("/hello", json!({}))],
+    );
+    assert_eq!(results, vec!["yay!", "yay!"]);
+}
+
+#[test]
+fn single_binop() {
+    let results = run_test(
+        "single_binop",
+        r#"
+        let containerless = require("containerless");
+        containerless.listen(function(req) {
+            let x = 12;
+            if(x > 2) {
+                containerless.respond("yay!");
+            } else {
+                containerless.respond("boo!");
+            }
+        });"#,
+        vec![("/hello", json!({}))],
+        vec![("/hello", json!({}))],
+    );
+    assert_eq!(results, vec!["yay!", "yay!"]);
+}
+
+#[test]
+fn nested_binops() {
+    let results = run_test(
+        "nested_binops",
+        r#"
+        let containerless = require("containerless");
+        containerless.listen(function(req) {
+            let x = 12;
+            if(x > 2 && x < 15) {
+                containerless.respond("yay!");
+            } else {
+                containerless.respond("boo!");
+            }
+        });"#,
+        vec![("/hello", json!({}))],
+        vec![("/hello", json!({}))],
+    );
+    assert_eq!(results, vec!["yay!", "yay!"]);
+}
