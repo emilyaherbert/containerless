c10d04e0bd0dc2f99e52559227fd2b53a06068a6
Arjun Guha <arjun@cs.umass.edu>
2020-04-30 16:07:48 -0400

    More robust testing script

--- rust/integration-tests/src/main.rs
+++ rust/integration-tests/src/main.rs
@@ -1,5 +1,71 @@
 mod test_runner;
 mod tests;
-fn main() {
-    println!("Run /docker/test.sh");
+
+use machine_ip;
+use nix::sys::signal::{self, Signal};
+use nix::unistd::Pid;
+use shared2::net;
+use std::convert::TryInto;
+use std::time::Duration;
+use tokio::process::{Child, Command};
+use tokio::signal::unix::{signal, SignalKind};
+
+async fn start_controller() -> Child {
+    let my_ip = machine_ip::get().expect("getting IP address");
+    return Command::new("cargo")
+        .arg("run")
+        .current_dir("../controller-agent") // TODO(arjun): fix
+        .env("LOG_LEVEL", "info")
+        .env("LOG_RSYSLOG_ADDR", format!("{}:514", my_ip))
+        .spawn()
+        .expect("spawning controller");
+}
+
+async fn run_tests() -> Child {
+    return Command::new("cargo")
+        .args(&["test", "--", "--test-threads=1"])
+        .spawn()
+        .expect("spawning cargo test")
+}
+
+fn child_pid(child: &Child) -> Pid {
+    return Pid::from_raw(child.id().try_into().unwrap());
+}
+
+#[tokio::main]
+async fn main() {
+
+    Command::new("git")
+        .args(&["checkout", "../dispatcher-agent/src/decontainerized_functions/mod.rs"])
+        .spawn()
+        .expect("spawning git")
+        .await
+        .expect("running git");
+
+    let http_client = reqwest::Client::new();
+    let controller_handle = start_controller().await;
+    let controller_pid = child_pid(&controller_handle);
+    net::poll_url_no_timeout(
+        &http_client,
+        "http://localhost/dispatcher/readinessProbe",
+        Duration::from_secs(1),
+    )
+    .await;
+
+    let tests_handle = run_tests().await;
+    let tests_pid = child_pid(&tests_handle);
+
+    // Send SIGTERM to "cargo test" when we receive SIGTERM.
+    tokio::task::spawn(async move {
+        let mut sigterm = signal(SignalKind::terminate()).expect("registering SIGTERM handler");
+        sigterm.recv().await;
+        signal::kill(tests_pid, Signal::SIGTERM).unwrap();
+    });
+
+    // Tests either terminate normally, or with SIGTERM.
+    tests_handle.await.unwrap();
+
+    // Send SIGTERM to controller. Note that we reach this line even if this
+    // process receives SIGTERM.
+    signal::kill(controller_pid, Signal::SIGTERM).unwrap();
 }
---
+++ rust/shared2/src/net.rs
@@ -0,0 +1,54 @@
+use std::time::{Instant, Duration};
+use std::fmt::Display;
+use reqwest::IntoUrl;
+use tokio::time;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+#[error("timeout error: {reason}")]
+pub struct TimeoutError {
+    pub reason: String,
+}
+
+async fn poll_url_internal<'a, U>(
+    client: &'a reqwest::Client,
+    url: U,
+    interval: Duration,
+    timeout: Option<Duration>) -> Result<(), TimeoutError> where
+    U : IntoUrl + Clone + Display {
+
+    let end_time = timeout.map(|t| Instant::now() + t);
+
+    loop {
+        if let Ok(resp) = client.get(url.clone()).send().await {
+            if resp.status().is_success() {
+                return Ok(());
+            }
+        }
+        if let Some(t) = end_time {
+            if Instant::now() >= t {
+                return Err(TimeoutError {
+                    reason: format!("polling {}", &url)
+                });
+            }
+        }
+        time::delay_for(interval).await;
+    }
+}
+
+pub async fn poll_url_no_timeout<'a, U>(
+    client: &'a reqwest::Client,
+    url: U,
+    interval: Duration) -> () where
+    U : IntoUrl + Clone + Display {
+    poll_url_internal(client, url, interval, None).await.unwrap();
+}
+
+pub async fn poll_url_with_timeout<'a, U>(
+    client: &'a reqwest::Client,
+    url: U,
+    interval: Duration,
+    timeout: Duration) -> Result<(), TimeoutError> where
+    U : IntoUrl + Clone + Display {
+    return poll_url_internal(client, url, interval, Some(timeout)).await;
+}
