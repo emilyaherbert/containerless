5ebe804559b74107a14703e921c99bd8069b564e
Emily Herbert <emilyherbert@cs.umass.edu>
2020-07-22 19:59:57 +0000

    Subcommand shims.

--- containerless/rust/cli/src/main.rs
+++ containerless/rust/cli/src/main.rs
@@ -7,59 +7,53 @@ use clap::Clap;
 /// This doc string acts as a help message when the user runs '--help'
 /// as do all doc strings on fields
 #[derive(Clap)]
-#[clap(version = "1.0", author = "Kevin K. <kbknapp@gmail.com>")]
+#[clap(name = "Containerless", version = "0.1", author = "Emily Herbert <emilyherbert@cs.umass.edu>")]
 struct Opts {
-    /// Sets a custom config file. Could have been an Option<T> with no default too
-    #[clap(short, long, default_value = "default.conf")]
-    config: String,
-    /// Some input. Because this isn't an Option<T> it's required to be used
-    input: String,
-    /// A level of verbosity, and can be used multiple times
-    #[clap(short, long, parse(from_occurrences))]
-    verbose: i32,
     #[clap(subcommand)]
     subcmd: SubCommand,
 }
 
 #[derive(Clap)]
 enum SubCommand {
-    #[clap(version = "1.3", author = "Someone E. <someone_else@other.com>")]
-    Test(Test),
+    CreateFunction(CreateFunction),
+    DeleteFunction(DeleteFunction),
+    ListFunctions(ListFunctions),
 }
 
-/// A subcommand for controlling testing
+/// Creates a function.
 #[derive(Clap)]
-struct Test {
+struct CreateFunction {
     /// Print debug info
     #[clap(short)]
-    debug: bool
+    name: String
 }
+
+/// Deletes a function.
+#[derive(Clap)]
+struct DeleteFunction {
+    /// Print debug info
+    #[clap(short)]
+    name: String
+}
+
+/// Lists all functions.
+#[derive(Clap)]
+struct ListFunctions { }
 
 fn main() {
     let opts: Opts = Opts::parse();
 
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    println!("Value for config: {}", opts.config);
-    println!("Using input file: {}", opts.input);
-
-    // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    match opts.verbose {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
-    }
-
     // You can handle information about subcommands by requesting their matches by name
     // (as below), requesting just the name used, or both at the same time
     match opts.subcmd {
-        SubCommand::Test(t) => {
-            if t.debug {
-                println!("Printing debug info...");
-            } else {
-                println!("Printing normally...");
-            }
+        SubCommand::CreateFunction(t) => {
+            println!("You called create function with {:?}!", t.name);
+        },
+        SubCommand::DeleteFunction(t) => {
+            println!("You called create function with {:?}!", t.name);
+        },
+        SubCommand::ListFunctions(_) => {
+            println!("You called list functions!");
         }
     }
 
