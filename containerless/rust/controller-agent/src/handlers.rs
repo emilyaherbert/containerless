575a223a5d610204bf85bab189edd302843f923c
Emily Herbert <emilyherbert@cs.umass.edu>
2020-07-28 21:04:16 +0000

    Better routes.

---
+++ containerless/rust/controller-agent/src/handlers.rs
@@ -0,0 +1,35 @@
+use bytes;
+use hyper::Response;
+
+use crate::common::*;
+use crate::compiler::Compiler;
+
+pub async fn ready() -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(Response::builder().status(200).body("Controller agent\n"))
+}
+
+pub async fn recv_trace(
+    name: String, trace: bytes::Bytes, compiler: Arc<Compiler>,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    info!(target: "controller", "received trace for {} ({} bytes)", &name, trace.len());
+    compiler.compile(name, trace);
+    Ok(Response::builder().status(200).body(""))
+}
+
+pub async fn ok_if_not_compiling_handler(
+    compiler: Arc<Compiler>,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(compiler.ok_if_not_compiling())
+}
+
+pub async fn restart_dispatcher_handler(
+    compiler: Arc<Compiler>,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(compiler.recompile_dispatcher().await)
+}
+
+pub async fn reset_dispatcher_handler(
+    compiler: Arc<Compiler>,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    Ok(compiler.reset_dispatcher().await)
+}
