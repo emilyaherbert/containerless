52427e3558dcd3a45516b6a0e587956cdb56f884
Emily Herbert <emilyherbert@cs.umass.edu>
2020-08-24 16:37:11 +0000

    actions

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+cd docker
+
+export EXAMPLES_PATH=$(dirname $(dirname $(realpath $0)))/examples
+export RUST_SRC_PATH=$(dirname $(dirname $(realpath $0)))/rust
+export CONTROLLER_PORT=7999
+export CONTROLLER_IP=$(hostname -I | cut -f 1 -d " ")
+export CONTROLLER_LOG_PATH=$(dirname $(realpath $0))
+
+echo "Clearing system state..."
+cp "${RUST_SRC_PATH}/dispatcher-agent/src/decontainerized_functions/template.txt" "${RUST_SRC_PATH}/dispatcher-agent/src/decontainerized_functions/mod.rs"
+rm "${RUST_SRC_PATH}/dispatcher-agent/src/decontainerized_functions/*.json" 2> /dev/null || true
+rm "${RUST_SRC_PATH}/dispatcher-agent/src/decontainerized_functions/*.rs" 2> /dev/null || true
+echo "System state cleared.\n"
+
+echo "Deploying on k8s..."
+envsubst < containerless.yaml | microk8s.kubectl apply --kubeconfig ~/.kube/config -f -
+echo "Deployed on k8s.\n"
+
+echo "Starting the controller."
+echo "This may take some time..."
+./controller.sh start
+echo "Controller is running at http://localhost/controller\n"
+
+echo "Waiting for dispatcher to come online."
+echo "This may take some time..."
+./poll-ready.sh http://localhost/dispatcher/readinessProbe 300 || exit 1
+echo "Dispatcher is running at http://localhost/dispatcher\n"
+
+echo "System is fully deployed.\n"
---
+++ .github/workflows/undeploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd docker
+
+./controller.sh stop
+
+export EXAMPLES_PATH=$(dirname $(dirname $(realpath $0)))/examples
+export CONTROLLER_LOG_PATH=$(dirname $(realpath $0))
+envsubst < containerless.yaml | microk8s.kubectl delete --kubeconfig ~/.kube/config --ignore-not-found -f -
+
+export RUST_SRC_PATH=$(dirname $(dirname $(realpath $0)))/rust
+echo "Clearing system state..."
+cp "${RUST_SRC_PATH}/dispatcher-agent/src/decontainerized_functions/template.txt" "${RUST_SRC_PATH}/dispatcher-agent/src/decontainerized_functions/mod.rs"
+rm "${RUST_SRC_PATH}/dispatcher-agent/src/decontainerized_functions/*.json" 2> /dev/null || true
+rm "${RUST_SRC_PATH}/dispatcher-agent/src/decontainerized_functions/*.rs" 2> /dev/null || true
+echo "System state cleared.\n"
