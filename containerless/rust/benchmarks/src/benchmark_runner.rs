4154737d4eb0f00122340b302e228790e81c8527
Emily Herbert <emilyherbert@cs.umass.edu>
2020-09-14 22:37:53 +0000

    Benchmarks work!

--- containerless/rust/benchmarks/src/benchmark_runner.rs
+++ containerless/rust/benchmarks/src/benchmark_runner.rs
@@ -1,6 +1,7 @@
 use crate::error::Error;
 
 use shared::common::*;
+use shared::cli;
 
 use tokio::process::Command;
 use std::process::Stdio;
@@ -43,32 +44,55 @@ async fn wrk_run(url: &str, wrk_options: WrkOptions) -> Result<(String, String),
     Ok((stdout, stderr))
 }
 
-async fn run_benchmark_async(name:&str, url: &str, wrk_options: WrkOptions) -> String {
+async fn run_benchmark_async(name:&str, js_code: &str, url: &str, wrk_options: WrkOptions) -> String {
     fn fail(err: Error) {
         error!(target: "benchmarks", "Error in the benchmark runner: {:?}", err);
         assert!(false, format!("{:?}", err));
+    }
+
+    async fn fail_and_delete(name: &str, err: Error) {
+        error!(target: "integration-tests", "Error in the test runner: {:?}", err);
+        let d: Result<std::process::Output, Error> = cli::containerless_delete(name).await;
+        if let Err(err) = d {
+            error!(target: "integration-tests", "Error in the test runner: {:?}", err);
+        }
+        assert!(false, format!("{:?}", err));
+    }
+
+    let empty = "".to_string();
+
+    // Create the function in Containerless
+    if let Err(err) = cli::containerless_create(name, js_code).await {
+        fail_and_delete(name, err).await;
+        return empty;
     }
 
     // Hammer the function using wrk
     let wrk_output = wrk_run(url, wrk_options.clone()).await;
     if let Err(err) = wrk_output {
         fail(err);
-        return "".to_string();
+        return empty;
     }
     let (stdout, _stderr) = wrk_output.unwrap();
+
+    // Delete everything!
+    if let Err(err) = cli::containerless_delete(name).await {
+        fail_and_delete(name, err).await;
+        return empty;
+    }
 
     // Save the results returned from wrk
     let mut file = File::create(format!("{}/../benchmarks/data/{}_{}.csv", ROOT.as_str(), name, wrk_options)).unwrap();
     if let Err(err) = file.write_all(stdout.as_bytes()) {
         fail(Error::IO(err));
-        return "".to_string();
+        return empty;
     }
 
     return "Done!".to_string();
 }
 
 #[allow(unused)]
-pub fn run_benchmark(name:&str, url: &str, wrk_options: WrkOptions) -> String {
+pub fn run_benchmark(name:&str, js_code: &str, url: &str, wrk_options: WrkOptions) -> String {
     let mut rt = tokio::runtime::Runtime::new().expect("creating Tokio runtime");
-    return rt.block_on(run_benchmark_async(name, url, wrk_options));
+    return rt.block_on(run_benchmark_async(name, js_code, url, wrk_options));
 }
--- containerless/rust/benchmarks/src/benchmarks.rs
+++ containerless/rust/benchmarks/src/benchmarks.rs
@@ -1,16 +1,40 @@
 #![cfg(test)]
-use super::benchmark_runner::run_benchmark;
-use crate::benchmark_runner::WrkOptions;
+use super::benchmark_runner;
 
 #[test]
 fn sanity_check() {
-    let wrk_options = WrkOptions {
+    let code =r#"
+        let containerless = require("containerless");
+        containerless.listen(function(req) {
+            containerless.respond("Hello, world!");
+        });"#;
+
+    let wrk_options = benchmark_runner::WrkOptions {
         connections: 10,
         duration: 10,
         threads: 4
     };
 
-    let result = run_benchmark("sanity", "http://google.com", wrk_options);
+    let result = benchmark_runner::run_benchmark("sanity", code, "http://google.com", wrk_options);
+
+    assert_eq!(result, "Done!".to_string());
+}
+
+#[test]
+fn hello_world() {
+    let code =r#"
+        let containerless = require("containerless");
+        containerless.listen(function(req) {
+            containerless.respond("Hello, world!");
+        });"#;
+
+    let wrk_options = benchmark_runner::WrkOptions {
+        connections: 10,
+        duration: 60,
+        threads: 4
+    };
+
+    let result = benchmark_runner::run_benchmark("helloworld", code, "http://localhost/dispatcher/helloworld", wrk_options);
 
     assert_eq!(result, "Done!".to_string());
 }
