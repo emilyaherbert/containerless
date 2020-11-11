6c108cd68931706de6d8d5b9b61dd063d02c8179
Arjun Guha <a.guha@northeastern.edu>
2020-11-11 15:48:53 +0000

    Removed stale script

--- containerless/docker/restart-controller.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-git checkout ../rust/dispatcher-agent/src/decontainerized_functions/mod.rs
-(cd ../rust && cargo build)
-microk8s.kubectl delete pod --selector "app=controller" -n containerless
-echo "You probably want to restart the dispatcher using ./restart-dispatcher.sh"
