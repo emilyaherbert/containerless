6e030f8159f04d1d7e02c45180c4a0f95d46dfee
Emily Herbert <emilyherbert@cs.umass.edu>
2020-11-17 03:07:35 +0000

    first pass at all benchmarks

--- containerless/rust/local-database-agent/src/applications/banking.rs
+++ containerless/rust/local-database-agent/src/applications/banking.rs
@@ -1,15 +1,19 @@
+use super::DB;
+
+use shared::response;
+
 use serde::{Deserialize, Serialize};
 use std::sync::atomic::{AtomicI64, Ordering};
 
 #[derive(Serialize, Deserialize, Debug)]
-struct AccountName {
+pub struct AccountName {
     name: String
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct InternalAccount {
-    name: String,
-    balance: AtomicI64
+pub struct InternalAccount {
+    pub name: String,
+    pub balance: AtomicI64
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -19,7 +23,33 @@ struct Account {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct AccountTransaction {
+pub struct AccountTransaction {
     transaction: String,
     mutation: Account
+}
+
+pub async fn begin_handler() -> Result<impl warp::Reply, warp::Rejection> {
+    response::ok_response("{ \"transaction\": \"135798642\" }".to_string())
+}
+
+pub async fn commit_handler(transaction: AccountTransaction, accounts_db: DB<InternalAccount>) -> Result<impl warp::Reply, warp::Rejection> {
+    let accounts_db = accounts_db.lock().await;
+
+    match accounts_db.get(&transaction.mutation.name) {
+        Some(acc) => {
+            let new_amount: i64 = (transaction.mutation.balance * 100.0) as i64;
+            acc.balance.store(new_amount, Ordering::SeqCst);
+            return response::ok_response("{ \"body\": \"Done!\" }".to_string());
+        },
+        None => return response::ok_response("{ \"body\": \"Account not found.\" }".to_string())
+    }
+}
+
+pub async fn balance_handler(account: AccountName, accounts_db: DB<InternalAccount>) -> Result<impl warp::Reply, warp::Rejection> {
+    let accounts_db = accounts_db.lock().await;
+    
+    match accounts_db.get(&account.name) {
+        Some(acc) => return response::ok_response(format!("{{ \"balance\": {} }}", (acc.balance.load(Ordering::SeqCst) / 100) as f64)),
+        None => return response::ok_response("{ \"body\": \"Account not found.\" }".to_string())
+    }
 }
--- containerless/rust/local-database-agent/src/applications/upload.rs
+++ containerless/rust/local-database-agent/src/applications/upload.rs
@@ -8,11 +8,11 @@ use std::collections::HashMap;
 
 pub async fn upload_handler(contents: HashMap<String, String>) -> Result<impl warp::Reply, warp::Rejection> {
     if !contents.contains_key("body") {
-        return response::error_response(r#"{ "error": "file not properly formatted\n" }"#.to_string());
+        return response::ok_response(r#"{ "error": "file not properly formatted\n" }"#.to_string());
     }
 
     if let Err(_err) = upload(contents.get("body").unwrap().to_string()).await {
-        return response::error_response(r#"{ "error": "error uploading file\n" }"#.to_string());
+        return response::ok_response(r#"{ "error": "error uploading file\n" }"#.to_string());
     }
 
     return response::ok_response(r#"{ "body": "Done uploading!\n" }"#.to_string());
